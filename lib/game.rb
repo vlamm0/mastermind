bd45f8cf27cedeb24575bd1ccbe8d891b675524a
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-08-02 13:09:32 -0400

    Start screen

--- lib/game.rb
+++ lib/game.rb
@@ -3,9 +3,14 @@ class Game
   attr_accessor :code, :mm, :turn, :cb
 
   def initialize
+    puts "***MASTER MIND***\n\n"
     self.mm = MasterMind.new
     self.cb = CodeBreaker.new
     self.code = mm.make_code
     self.turn = 0
   end
+
+  # def start
+  #   puts "***MASTER MIND***\n\n"
+  # end
 end
